32aba2ccc2f4f4d50d66edfa7b07f26c9c779aa7
Enrique Hernandez <56319823+LaSensacionDelBloque@users.noreply.github.com>
2021-05-08 22:37:23 -0500

    ini

--- app/src/main/java/com/example/desarrollopersonal/Temas.java
+++ app/src/main/java/com/example/desarrollopersonal/Temas.java
@@ -2,7 +2,9 @@ package com.example.desarrollopersonal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Temas extends AppCompatActivity {
 
@@ -10,5 +12,25 @@ public class Temas extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_temas);
+    }
+    public void Tecnicas(View view) {
+        Intent siguiente = new Intent(this, Tecnicas.class);
+        startActivity(siguiente);
+
+    }
+    public void Lectura(View view) {
+        Intent siguiente = new Intent(this, Lectura.class);
+        siguiente.putExtra("dato","Ejercicios para mejorar tu lectura.pdf");
+        startActivity(siguiente);
+    }
+    public void Habitos(View view) {
+        Intent siguiente = new Intent(this, Lectura.class);
+        siguiente.putExtra("dato","Creacion de habitos.pdf");
+        startActivity(siguiente);
+    }
+    public void Word(View view) {
+        Intent siguiente = new Intent(this, Lectura.class);
+        siguiente.putExtra("dato","Mejorar trabajos en word.pdf");
+        startActivity(siguiente);
     }
 }
